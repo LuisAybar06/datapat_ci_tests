d52756cbf04b12a1912438309c3d76c141913ce2
PC <aluisaybar@gmail.com>
2024-06-08 16:39:55 -0500

    prueba_12

--- my_app/main.py
+++ my_app/main.py
@@ -1,2 +1,16 @@
-def add(a, b):
-    return a + b
+def process_numbers(a, b, c, d, e, f):
+    # Realiza varias operaciones con los números
+    sum_all = a + b + c + d + e + f
+    product_all = a * b * c * d * e * f
+    average = sum_all / 6
+    max_num = max(a, b, c, d, e, f)
+    min_num = min(a, b, c, d, e, f)
+    
+    result = {
+        'sum': sum_all,
+        'product': product_all,
+        'average': average,
+        'max': max_num,
+        'min': min_num
+    }
+    return result

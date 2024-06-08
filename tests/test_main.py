9ad838c40f54be8e0e18e41b4a4c775a6e898d39
PC <aluisaybar@gmail.com>
2024-06-08 15:59:06 -0500

    prueba_08

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,8 +3,8 @@ from my_app.main import add
 
 class TestMain(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(1, 2), 3) 
+        self.assertEqual(add(-1, 1), 0) 
         self.assertEqual(add(0, 0), 0)
 
 if __name__ == '__main__':
